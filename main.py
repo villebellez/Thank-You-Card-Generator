a852b9ac2c5cc2343d46d38d99b87aec9e4c5da2
MJ Belleville <villebellez@outlook.com>
2023-03-10 14:13:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+print("""
+ ______  __ __   ____  ____   __  _      __ __   ___   __ __         __   ____  ____   ___   _____
+|      ||  |  | /    ||    \ |  |/ ]    |  |  | /   \ |  |  |       /  ] /    ||    \ |   \ / ___/
+|      ||  |  ||  o  ||  _  ||  ' /     |  |  ||     ||  |  |      /  / |  o  ||  D  )|    (   \_ 
+|_|  |_||  _  ||     ||  |  ||    \     |  ~  ||  O  ||  |  |     /  /  |     ||    / |  D  \__  |
+  |  |  |  |  ||  _  ||  |  ||     \    |___, ||     ||  :  |    /   \_ |  _  ||    \ |     /  \ |
+  |  |  |  |  ||  |  ||  |  ||  .  |    |     ||     ||     |    \     ||  |  ||  .  \|     \    |
+  |__|  |__|__||__|__||__|__||__|\_|    |____/  \___/  \__,_|     \____||__|__||__|\_||_____|\___|
+  A   G E N E R A T O R   F O R   P A R E N T S   W H O   J U S T   D O N T   H A V E   T I M E 
+  """)
+
+print("Do you hate having to write thank you notes after every single one of your children's birthdays? Same!\n"
+      "It's repetitive and tedious, and many of us just don't have time for it. Thankfully, there's a way to make this process a bit easier.\n")
+
+print("This will be the generic letter that we are sending:\n\n"
+      "\"Dear [name],\n\n"
+      "Thank you so much for attending [child's name]'s birthday party and for your generous gift! [child's name] is going to absolutely love it. "
+      "We hope you had a great time and that we'll see you at the next one!\n\n"
+      "Love,\n"
+      "The [family name]'s\" \n")
+
+print("So let's start filling it out!")
+childs_name = input("First, what is your child's first name? ")
+family_name = input("What is your family's last name? ")
+
+# TODO: see if I can figure out how to upload files, or just have them point where the list of names is in their system.
+
+# TODO: Create a letter using starting_letter.txt
+
+# TODO: for each name in recipients.txt, replace the [name], [child's name], and [family name] placeholders.
+
+# TODO: Save the letters in the folder "ReadyToSend"
+
+
+
